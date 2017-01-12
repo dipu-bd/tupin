6947982ffbd98cbce894a8d34e816caaa8c44ba2
dipu-bd <dipu.sudipta@gmail.com>
2017-01-13 00:27:01 +0600

    Added string maipulator

---
+++ lib/Lexer.hpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+#include "Location.hpp"
+#include "Environment.hpp"
+#include "StringBuilder.hpp"
+
+Location loc;
+Environment env;
+ 
+void yyerror(const char *s)
+{
+    fprintf(stderr, "# %s:%d:%d: %s %s\n", env.cur(), loc.line, loc.col, s, yytext);
+}
+
+void checkNumber(const char *s, int base = 10)
+{
+    if (base >= 2)
+    {
+        long long n = std::strtoll(s, 0, base);
+        printf("~ INTEGER: %lld [base=%d] \"%s\"\n", n, base, yytext);
+    }
+    else
+    {
+        double n = std::strtod(s, NULL);
+        printf("~ FLOAT: %g \"%s\"\n", n, yytext);
+    }
+}
+
+void checkString(const char *s)
+{
+    printf("~ STRING: %s\n", s);
+}
+
+void checkIdentifier(const char *s)
+{
+    printf("~ ID: %s\n", s);
+}
+
+void escapeToChar(char *str, int &pos)
+{
+    while (pos > 0 && str[pos - 1] != '\\')
+        pos--;
+
+    long ch = str[pos];
+    if (str[pos] == 'x' || str[pos] == 'X')
+    {
+        ch = std::strtol(str + pos + 1, 0, 16);
+    }
+    else if (isdigit(str[pos]))
+    {
+        ch = std::strtol(str + pos, 0, 8);
+    }
+    const char *real = "abfnrtv";
+    const char *rplc = "\a\b\f\n\r\t\v";
+    for (int i = strlen(real) - 1; i >= 0; --i)
+    {
+        if (ch == real[i])
+        {
+            ch = rplc[i];
+            break;
+        }
+    }
+    if (ch < 0 || ch > 255)
+    {
+        yyerror("Bad hexadecimal escape sequence");
+        ch = 0;
+    }
+    // set character
+    str[pos - 1] = ch;
+    str[pos] = 0;
+}
--- lib/Locatoin.hpp
+++
@@ -1,9 +0,0 @@
-
-struct Location
-{
-    int line;
-    int col;
-    Location(int lno = 0, int cno = 0) : line(lno), col(cno)
-    {
-    }
-};

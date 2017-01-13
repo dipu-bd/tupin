a36751b1556b8c43a10cc892aa06a9b115780901
dipu-bd <dipu.sudipta@gmail.com>
2017-01-13 19:29:57 +0600

    Hidden build directory

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-rm -rf "build/" &&
-mkdir "build" && 
-cp -R "src/lib/" "build/" &&
-flex -o"build/Lexer.cpp" "src/Lexer.l" && 
-g++ -std=gnu++11 -o "build/Program" "build/Lexer.cpp"
+rm -rf ".build/" &&
+mkdir ".build" && 
+cp -R "src/lib/" ".build/" &&
+flex -o".build/Lexer.cpp" "src/Lexer.l" && 
+g++ -std=gnu++11 -o ".build/Program" ".build/Lexer.cpp"

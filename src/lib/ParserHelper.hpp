bf12ee470097b8001cbf70a0b36bed57827e8cca
dipu-bd <dipu.sudipta@gmail.com>
2017-01-15 00:39:22 +0600

    Separated main function from Parser

--- src/lib/ParserHelper.hpp
+++ src/lib/ParserHelper.hpp
@@ -14,3 +14,17 @@ void yyerror(const char *msg)
 {
     yyerror(msg, -1, -1);
 }
+
+int MainFunction(int argc, char** argv)
+{
+    auto begin = std::chrono::high_resolution_clock::now();
+    init(argc, argv);    
+
+    int res = yyparse();
+
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end-begin).count();
+    fprintf(stderr, "\n\nExited with status %d.\nRuntime: %f ms\n", res, duration / 1e6);
+
+    return res;
+}

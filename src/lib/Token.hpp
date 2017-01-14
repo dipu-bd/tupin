3f7c7c9ffa755478dbd9191a9b7d1fea1397471b
dipu-bd <dipu.sudipta@gmail.com>
2017-01-14 22:42:45 +0600

    Added Token class

---
+++ src/lib/Token.hpp
@@ -0,0 +1,44 @@
+#include <iostream> 
+using namespace std;
+
+struct Token
+{
+    string file;
+    string outfile;
+    int type;
+    string val;
+    int line;
+    int column;
+
+    Token() : line(0), 
+              column(0), 
+              file(""), 
+              outfile(""), 
+              val(""), 
+              type(0) 
+    { 
+
+    }
+
+    void clear() 
+    {
+        column = 0;
+    } 
+    void update(int _line, int len)
+    {
+        column += len;
+        line = _line;
+    }    
+
+    Token copy()
+    {
+        Token tok;
+        tok.val = vald.data();
+        tok.type = type;
+        tok.line = line;
+        tok.column = column;
+        tok.file = file.data();
+        tok.outfile = outfile.data();
+        return tok;
+    }
+};
